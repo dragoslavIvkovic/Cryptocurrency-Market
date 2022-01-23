83e0688825c6bb32bbb532c890bef821b746d062
Dragoslav Ivkovic <Ivkovic.dragoslav.dragi@gmail.com>
2022-01-23 19:54:52 +0100

    login navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,69 +4,86 @@ import {
   MenuItem,
   Select,
   Toolbar,
-  Typography,
-} from "@material-ui/core";
+  Typography
+} from '@material-ui/core'
 import {
   createTheme,
   makeStyles,
-  ThemeProvider,
-} from "@material-ui/core/styles";
-import { Box, Grid } from "@mui/material";
-import React, { useContext } from "react";
-import { Link } from "react-router-dom";
-import "../../_styles/Navbar.scss";
-import { StateContext } from "../../context/GlobalState";
-import AuthModal from "../Auth/AuthModal";
+  ThemeProvider
+} from '@material-ui/core/styles'
+import { Avatar, Button } from '@material-ui/core'
+import { Box, Grid } from '@mui/material'
+import React, { useContext, useState } from 'react'
+import { Link } from 'react-router-dom'
+import '../../_styles/Navbar.scss'
+import { signOut } from 'firebase/auth'
+import { StateContext } from '../../context/GlobalState'
+import AuthModal from '../Auth/AuthModal'
+import { auth, db } from '../../firebase'
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   title: {
     flex: 1,
-    color: "gold",
-    fontFamily: "Montserrat",
-    fontWeight: "bold",
-    cursor: "pointer",
-  },
-}));
+    color: 'gold',
+    fontFamily: 'Montserrat',
+    fontWeight: 'bold',
+    cursor: 'pointer'
+  }
+}))
 
 const darkTheme = createTheme({
   palette: {
     primary: {
-      main: "#fff",
+      main: '#fff'
     },
-    type: "dark",
-  },
-});
+    type: 'dark'
+  }
+})
 
-
-
-
-export default function Navbar() {
-     const classes = useStyles();
-  const {  user } = useContext(StateContext);
-
+export default function Navbar () {
+  const classes = useStyles()
+  const { user, setAlert } = useContext(StateContext)
+ 
+  const logOut = () => {
+    signOut(auth)
+    setAlert({
+      open: true,
+      type: 'success',
+      message: 'Logout Successfully !'
+    })
+  }
 
   return (
     <nav>
       <Box
         sx={{
-          mx: "50px",
-          mt: "10px",
-          display: "flex-inline",
-          flexDirection: "row",
+          mx: '50px',
+          mt: '10px',
+          display: 'flex-inline',
+          flexDirection: 'row'
         }}
       >
-        <Link className="navbar-link" to="/">
+        <Link className='navbar-link' to='/'>
           Home
         </Link>
 
-        <Link className="navbar-link" style={{ float: "right" }} to="watchlist">
+        <Link className='navbar-link' style={{ float: 'right' }} to='watchlist'>
           Watchlist
         </Link>
-        <Grid className="navbar-link" style={{ float: "right" }} >
-          {user ? "Logout" : <AuthModal />}
+        <Grid className='navbar-link' style={{ float: 'right' }}>
+          {user ? (
+            <Button
+              variant='contained'
+              className={classes.logout}
+              onClick={logOut}
+            >
+              Log Out
+            </Button>
+          ) : (
+            <AuthModal />
+          )}
         </Grid>
-        
       </Box>
     </nav>
-  );
+  )
 }
